d425d744153a09b3b0694333f2fe634f5904f632
Timothy Cope <kirbycope@gmail.com>
2025-10-11 01:05:34 -0700

    eod

--- scenes/minecraft/main.gd
+++ scenes/minecraft/main.gd
@@ -1,11 +1,9 @@
 extends Node3D
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+@onready var player = $Player
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+## Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	# Capture the mouse
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
