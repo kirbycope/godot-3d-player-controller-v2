077d8e76f08c6135b834d85823c9afdffc874671
Timothy Cope <kirbycope@gmail.com>
2025-10-17 11:03:04 -0700

    wip

--- scenes/zelda_breath_of_the_wild/main.gd
+++ scenes/zelda_breath_of_the_wild/main.gd
@@ -10,7 +10,7 @@ func _ready() -> void:
 	#player.camera.toggle_perspective() # Run in _ready() to start in 1st person
 	player.camera.lock_camera = false
 	player.camera.lock_perspective = false
-	player.enable_climbing = false
+	player.enable_climbing = true
 	player.enable_crawling = true
 	player.enable_crouching = true
 	player.enable_double_jumping = false
