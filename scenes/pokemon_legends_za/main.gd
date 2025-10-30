88ce286d751b40f52e3af991235757a8ab0a5981
Timothy Cope <kirbycope@gmail.com>
2025-10-30 15:21:49 -0700

    Update main.gd

--- scenes/pokemon_legends_za/main.gd
+++ scenes/pokemon_legends_za/main.gd
@@ -31,3 +31,20 @@ func _ready() -> void:
 	player.lock_movement_x = false
 	player.lock_movement_y = false
 	player.lock_movement_z = false
+
+
+## Called when there is an input event.
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed(player.controls.button_5):
+		player.play_locked_animation("Standing_Throwing_Right/mixamo_com")
+		await get_tree().create_timer(0.8).timeout
+		var tennis_ball = $TennisBall.duplicate()
+		tennis_ball.get_node("PlayerDetection").monitoring = false
+		add_child(tennis_ball)
+		tennis_ball.scale = Vector3.ONE
+		var force_direction = -player.camera.global_transform.basis.z.normalized()
+		var spawn_offset = player.up_direction * 1.5 + force_direction * 0.5
+		tennis_ball.global_position = player.global_position + spawn_offset
+		tennis_ball.apply_impulse(force_direction * 10.0, Vector3.ZERO)
+		await get_tree().create_timer(0.1).timeout
+		tennis_ball.get_node("PlayerDetection").monitoring = true
